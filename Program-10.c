5408cc8cc95589145e505e808990880c6282e3fc
randomplayer007 <84766541+randomplayer007@users.noreply.github.com>
2021-06-20 15:39:26 +0530

    done

--- Program-10.c
+++ Program-10.c
@@ -28,12 +28,12 @@ int main()
         
         if (arr[i] >large  )//missing code
         {
-            large= arr[];//missing code
+            large= arr[i];//missing code
         }
     }
 
-    printf("The largest element of the array = %d", large);
-    printf("The smallest element of the array = %d",);
+    printf("\nThe largest element of the array = %d", large);
+    printf("\nThe smallest element of the array = %d",small);
 
     return 0;
 }
